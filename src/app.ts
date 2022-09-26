0a6caa9e9adb525eba54a56799ff80b2c345e4be
KaykyTavares <kayky.programmer@gmail.com>
2022-09-26 18:52:50 -0300

    initializing express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log("deu boa!!!")
+import express from "express";
+import config from "config";
+
+const app = express();
+
+app.use(express.json());
+
+const port = config.get<string>("port");
+
+app.listen(port, async () => {
+  console.log(`Aplicação está funcionando na porta: ${port}`);
+});
