4431139d0f9dfe975235967883a657648d3c7471
KaykyTavares <kayky.programmer@gmail.com>
2022-09-26 22:47:03 -0300

    adding test routel

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import config from "config";
+import router from "./router"
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/", router)
 
 const port = config.get<string>("port");
 
