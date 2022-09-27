0d800a51c53e14c63c6f1ff4440a37d82b293dfd
KaykyTavares <kayky.programmer@gmail.com>
2022-09-27 13:36:36 -0300

    configuring  morgan

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 require("dotenv").config();
 import Logger from "../config/logger";
+import morganMiddleware from "./middleware/morganMiddleware";
 import express from "express";
 import config from "config";
 import router from "./router";
@@ -8,6 +9,7 @@ import db from "../config/db";
 const app = express();
 
 app.use(express.json());
+app.use(morganMiddleware)
 app.use("/api/", router);
 
 const port = config.get<string>("port");
